d9d26331f51f6bbe5e7a7f54b8ce52299b440044
Alexander King <alexander@Akingsquest.com>
2016-08-03 15:03:28 -0400

    Update PlayerGrapple.cs
    Moving keycode check outside of Shoot function (so something else could call it, for example)

--- Assets/Scripts/PlayerGrapple.cs
+++ Assets/Scripts/PlayerGrapple.cs
@@ -12,17 +12,19 @@ public class PlayerGrapple : MonoBehaviour {
 
 	void Update ()
 	{
-		shoot ();
+		if (Input.GetKeyDown(KeyCode.E)) {
+			shoot ();
+		}
 	}
 
 	void shoot ()
 	{
-		if (Input.GetKeyDown(KeyCode.E)) {
+		//if (Input.GetKeyDown(KeyCode.E)) {
 			Vector2 bulletDirection = (spawnPoint.position - transform.position).normalized;
 			GameObject bulletPrefab = Instantiate (bullet, spawnPoint.position, transform.rotation) as GameObject;
 			Rigidbody2D bulletRigidBody2D = bulletPrefab.GetComponent<Rigidbody2D> ();
 			bulletRigidBody2D.AddForce (bulletDirection * bulletSpeed, ForceMode2D.Impulse);
-		}
+		//}
 	}
 		
 }
