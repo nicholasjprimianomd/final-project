8352449527ae715d9e3e713e27cf7ffef1b35539
Nicholas Primiano <njp280@nyu.edu>
2016-08-06 00:41:22 -0400

    no message

---
+++ Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : MonoBehaviour
+{
+
+	public void loadGameLevel ()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
