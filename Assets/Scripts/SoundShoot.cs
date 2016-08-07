1e2350858532b92861467586276a396352ad9717
Rachel Clayton <rachelclayton95@gmail.com>
2016-08-07 17:56:54 -0400

    background and grapple sounds, assets in place for background, lake, border, logs, and moving platform (vines)

---
+++ Assets/Scripts/SoundShoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundShoot : MonoBehaviour { //grapple sound
+	public AudioSource myAudio; //makes available in inspector
+	//can deckare mulitple audio sources
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKeyDown (KeyCode.E) ) {
+//			myAudio.Play();
+			// ^ sound overlay
+			// \/shot cannot be interrupted
+			myAudio.PlayOneShot ( myAudio.clip ); //use for footsteps
+		}
+	}
+}
