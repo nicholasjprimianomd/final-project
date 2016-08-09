c39a17545426d10757ab33b85e8d6232204ca557
Nicholas Primiano <njp280@nyu.edu>
2016-08-09 18:57:55 -0400

    no message

--- Assets/Scripts/Loren/waterDeath.cs
+++ Assets/Scripts/Loren/waterDeath.cs
@@ -1,14 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class waterDeath : MonoBehaviour {
+public class waterDeath : MonoBehaviour
+{
 
+	private bool takingDamage;
+	private float currentTime;
+	private float startTime;
+	public float coolDown = .25f;
+	public int waterDamage = 3;
 
-	void OnTriggerEnter2D( Collider2D activator){
+	void Update ()
+	{
+		currentTime = Time.time;
 
-		if (activator.GetComponent<PlayerGrapple> () != null && activator.gameObject.GetComponent<PlayerGrapple>().isLerping == false) {
-			Destroy (activator.gameObject);
+	}
+
+	void OnTriggerStay2D (Collider2D activator)
+	{
+		if (activator.GetComponent<PlayerGrapple> () != null && activator.gameObject.GetComponent<PlayerGrapple> ().isLerping == false) {
+			takeDamage (activator);
 		}
 	}
 
+	void OnTriggerEnter2D (Collider2D activator)
+	{
+		startTime = currentTime;
+	}
+
+	void takeDamage (Collider2D activator)
+	{
+		if (currentTime - startTime > coolDown) {
+			if (activator.GetComponent<PlayerTakeDamage> ().currentShield > waterDamage) {
+				activator.GetComponent<PlayerTakeDamage> ().currentShield -= waterDamage * Mathf.CeilToInt (Time.deltaTime)/*Mathf.CeilToInt (Time.deltaTime / 100)*/;
+
+				startTime = currentTime;
+			} else {
+				activator.GetComponent<PlayerTakeDamage> ().currentHealth -= waterDamage * Mathf.CeilToInt (Time.deltaTime) /*Mathf.CeilToInt (Time.deltaTime / 100)*/;
+
+				startTime = currentTime;
+			}
+		}
+	}
 }
--- Assets/Scripts/ZombieMove.cs
+++ Assets/Scripts/ZombieMove.cs
@@ -19,8 +19,9 @@ public class ZombieMove : MonoBehaviour
 
 	void Update ()
 	{
-		destination = GameObject.FindGameObjectWithTag ("Player").transform;
-
+		if (GameObject.FindGameObjectWithTag ("Player") != null) {
+			destination = GameObject.FindGameObjectWithTag ("Player").transform;
+		}
 		if (Vector3.Distance (transform.position, destination.position) > .5f && canMove && !stun) {
 			transform.position += Vector3.Normalize (destination.position - transform.position) * Time.deltaTime * zombieMoveSpeed;
 		}
